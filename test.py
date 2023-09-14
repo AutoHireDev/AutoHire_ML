794ddde4a741f8d819f65ea08474884f74c875b7
AutoHireDev <130341487+AutoHireDev@users.noreply.github.com>
2023-09-14 23:10:20 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,38 @@
+import cv2
+import os
+import glob
+import numpy as np
+import time
+import pickle
+from Facerec import Facerec
+import torch
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+sfr = Facerec()
+score = 0
+count = 0
+
+
+@app.route("/api/load", methods=["POST"])
+def load_face():
+    return sfr.load_encoding_images("/saved")
+
+
+@app.route("/api/check", methods=["POST"])
+def check_face():
+    img = cv2.imread("/check_path/image.jpg")
+    score += sfr.detect_known_faces(img)
+    count += 1
+    return True
+
+
+@app.route("/api/eval", methods=["POST"])
+def eval():
+    return (score / count) * 100
+
+
+@app.route("/api/reset", methods=["POST"])
+def reset():
+    return sfr.clear_encodings()
