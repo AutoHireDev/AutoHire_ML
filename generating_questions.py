e009808930c1d4e0676374a7ccc7de5a01851d67
Tharun <77895048+tharun-vemula@users.noreply.github.com>
2023-12-05 22:35:19 +0530

    Update generating_questions.py

--- generating_questions.py
+++ generating_questions.py
@@ -1,28 +1,24 @@
 import openai
 import json,os
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-openai.api_key = os.getenv("OPENAI_KEY") 
+openai.api_key = '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'
 
 app = Flask(__name__)
 CORS(app)
 
 def generate_questions_and_answers(topic):
-    prompt = f"Generate 2 interview questions and 3 possible answers about the {topic} and return the response as an array of python objects with question and answer_variants as properties."
+    prompt = f"Generate 2 interview questions and 3 possible answers variants for each question about the {topic} and return these questions and answers in JSON format only as an array of python objects with 'question' and 'answer_variants' as properties. Enclose property names in double quotes. Reply with only the answer in JSON form and include no other commentary strictly. Do not include double quoutes in answers."
 
     response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="gpt-3.5-turbo-instruct",
         prompt=prompt,
         max_tokens=1000 
     )
-    print(response.choices[0])
+    
     generated_text = response.choices[0].text
-    print(generated_text)
-    print(type(generated_text))
-    print(json.loads(generated_text))
-    print(type((json.loads(generated_text))))
     return json.loads(generated_text)
 
 @app.route('/generate', methods=['GET'])
@@ -31,7 +27,6 @@ def generate_topic_questions_answers():
 
     questions_and_answers = generate_questions_and_answers(topic)
     return questions_and_answers
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5008)
