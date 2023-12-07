37e4e1b1496e23f7fcc603e7d0a8cd9f085e5605
Tharun <77895048+tharun-vemula@users.noreply.github.com>
2023-12-07 20:02:03 +0530

    Update Text_similarity.py

--- Text_similarity.py
+++ Text_similarity.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from sentence_transformers import SentenceTransformer, util
 import json
 
 app = Flask(__name__)
+CORS(app)
 model = SentenceTransformer("bert-base-nli-mean-tokens")
 
 
