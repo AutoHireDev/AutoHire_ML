23195b83892ad46bb0c93decb07ba8533ebe871d
AutoHireDev <130341487+AutoHireDev@users.noreply.github.com>
2023-12-25 21:52:02 +0530

    Update Text_similarity.py

--- Text_similarity.py
+++ Text_similarity.py
@@ -1,1 +1,25 @@
 
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from sentence_transformers import SentenceTransformer, util
+import json
+
+app = Flask(__name__)
+CORS(app)
+model = SentenceTransformer("bert-base-nli-mean-tokens")
+
+
+@app.route("/api/sbert-encode", methods=["POST"])
+def sbert_encode():
+    data = request.get_json()
+    sentences = data["sentences"]
+
+    sentence_embeddings = model.encode(sentences)
+    encoded_sentences = sentence_embeddings.tolist()
+    result = util.cos_sim(encoded_sentences[0], encoded_sentences[1])
+
+    return jsonify(result.tolist())
+
+
+if __name__ == "__main__":
+    app.run()
